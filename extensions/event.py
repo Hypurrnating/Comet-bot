2a059425dfa54104649c4f52fc4bc4bbb9b4bf30
Hypurrnating <imadyounis2004@gmail.com>
2024-08-09 23:16:51 +0300

    wheww

--- extensions/event.py
+++ extensions/event.py
@@ -3,15 +3,63 @@ import discord.ext
 import discord.ext.commands
 from discord import Interaction, app_commands
 import asyncio
+import typing
+import validators
+import urllib
+import urllib.parse
+
+from discord.utils import MISSING
 
 class event_cog(discord.ext.commands.Cog):
     def __init__(self, bot: discord.ext.commands.Bot) -> None:
         self.bot = bot
         super().__init__()
 
+    def build_internal_join_url(self, guild_id: int, event_id: int, event_title: str = None):
+        return f'https://donut.imady.pro/event/{event_title}/{guild_id}/{event_id}'
+
     event_group = discord.app_commands.Group(name='event', description='Commands that help you manage sessions')
 
-    
+    @discord.app_commands.command(name='new',
+                                  description='Start a new event', 
+                                  extras=dict())
+    @discord.app_commands.describe(title='Title of the event.',
+                                   description='What is this event about?',
+                                   post_to='Where should the event be announced?',
+                                   use_discord_event='Whether or not a discord event should be started alongside')
+    async def event_new(self, interaction: discord.Interaction,
+                        title: str,
+                        description: str,
+                        type: typing.Literal['URL Join', 'Instructions'],
+                        post_to: discord.TextChannel,
+                        thumbnail: discord.Attachment = None,
+                        use_discord_event: bool = True,
+                        ):
+        modal = self.event_instructions_modal(title='Now tell users how to join', type=type)
+        await interaction.response.send_modal(modal)
+        await modal.wait()
+        if not modal._join.value: return
+        if modal._join.value == 'URL Join':
+            if not validators.url(modal._join.value):
+                await interaction.followup.send(f'The URL you provided is invalid: {modal._join.value}')
+        
+        url_safe_title = urllib.parse.quote(title.replace(' ', '_'))
+        internal_join_url = self.build_internal_join_url(guild_id=interaction.guild.id, event_id=interaction.id, event_title=url_safe_title)
+        
+
+    class event_instructions_modal(discord.ui.Modal):
+        def __init__(self, title: str, type: typing.Literal['URL Join', 'Instructions'], timeout: float | None = None, custom_id: str = None) -> None:
+            if type == 'URL Join':
+                _join = discord.ui.TextInput(label='Enter URL', style=discord.TextStyle.short)
+            elif type == 'Instructions':
+                _join = discord.ui.TextInput(label='Enter join instructions', style=discord.TextStyle.long)
+
+            super().__init__(title=title, timeout=timeout, custom_id=custom_id)
+        
+        async def on_submit(self, interaction: Interaction) -> None:
+            await interaction.response.defer()
+
+
 
 
 async def setup(bot):
--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import aiosqlite.cursor
 import discord
 import discord.ext.commands  # Doesn't generate the docs if you dont import this
 import asyncpg
+import redis
 import aiosqlite
 import logging
 import os
 import platform
 import pathlib
 import asyncio
+import typing
 from multiprocessing.connection import Listener
 from datetime import datetime
 from dotenv import load_dotenv
@@ -27,23 +29,15 @@ class Donut(discord.ext.commands.Bot):
 
         self.bot = self     # Lets me use both `self` and `self.bot` in functions
         self.quart = self.QuartWeb(self)
-        discord.utils.setup_logging(level=logging.INFO)
-        logging.getLogger('discord.gateway').setLevel(30)   # Stops a flood of "gate RESUMED" messages
-
         self.webhooks = dict()
         self.errors = self.errors()
-        asyncio.create_task(self.setup_db())
+        discord.utils.setup_logging(level=logging.INFO)
+        logging.getLogger('discord.gateway').setLevel(30)   # Stops a flood of "gate RESUMED" messages      
 
         for x in pathlib.Path(f'./extensions').iterdir():
             if x.is_file():
                 asyncio.create_task(self.load_extension(f'extensions.{x.name.split(".")[0]}'))
 
-    def get_db_loc(self):
-        print(f'Running on: {platform.system()}')
-        if platform.system() == 'Windows':
-            return '.db'
-        if platform.system() == 'Linux':
-            return '/s/.db'
 
     async def create_tables(self):
         await (await self.sqlite.cursor()).execute('CREATE TABLE IF NOT EXISTS channel_webhooks(id INTEGER PRIMARY KEY AUTOINCREMENT, channel_id BIGINT, url VARCHAR(1500))')
@@ -51,9 +45,6 @@ class Donut(discord.ext.commands.Bot):
         await (await self.sqlite.cursor()).execute('CREATE TABLE IF NOT EXISTS stars(id INTEGER PRIMARY KEY AUTOINCREMENT, guild_id BIGINT, channel_id BIGINT, message_id BIGINT, user_id BIGINT)')
         #await (await self.sqlite.cursor()).execute('CREATE TABLE IF NOT EXISTS events(id INTEGER PRIMARY KEY AUTOINCREMENT, )')
 
-    async def setup_db(self):
-        self.sqlite = await aiosqlite.connect(self.get_db_loc())
-        await self.create_tables()
 
     async def on_ready(self):
         await self.tree.sync()
@@ -68,6 +59,17 @@ class Donut(discord.ext.commands.Bot):
                 """Exception for whenever there is an error relating managing webhooks"""
                 pass
             self.WebhookError = WebhookError
+
+    async def grab_webhook(self, channel: discord.TextChannel) -> discord.Webhook | None:
+        if not isinstance(channel, discord.TextChannel):
+            return
+        
+        channel_webhooks = await channel.webhooks()
+        for webhook in channel_webhooks:
+            if await self.is_bot_webhook(webhook):
+                return webhook
+        
+        return await channel.create_webhook(name=self.user.name, avatar=await self.user.avatar.read())
     
 
     async def is_bot_webhook(self, webhook: discord.Webhook) -> bool:
@@ -87,7 +89,7 @@ class Donut(discord.ext.commands.Bot):
         
         return True
 
-    async def cache_channel_webhook(self, channel: discord.TextChannel, webhook: discord.Webhook = None):
+    async def _store_channel_webhook(self, channel: discord.TextChannel, webhook: discord.Webhook = None):
         bot_webhook = webhook if webhook else (bot.webhooks[channel.id] if bot.webhooks.get(channel.id) else None)
         if not bot_webhook:
             raise Exception('No webhook passed, and none in channel attribute')
@@ -123,18 +125,18 @@ class Donut(discord.ext.commands.Bot):
             return self.webhooks[channel.id]
         
         else:
-            # get a cached webhook url from sqlite
+            # get a stored webhook url from sqlite
             resp = await (await (await self.sqlite.cursor()).execute("SELECT * FROM channel_webhooks WHERE channel_id = ?", (channel.id,))).fetchone()
 
-            # if it does exist in sqlite cache then create the partial webhook, and then create the complete one by .fetch()
+            # if it does exist in sqlite storage then create the partial webhook, and then create the complete one by .fetch()
             if resp:
                 bot_webhook = await (discord.Webhook.from_url(resp['url'])).fetch()
 
-            # if it doesn't exist in sqlite cache then fetch all webhooks in channel, if found use it and cache it, if not found create one and cache it.
+            # if it doesn't exist in sqlite storage then fetch all webhooks in channel, if found use it and store it, if not found create one and store it.
             if not resp: 
                 for webhook in (await channel.webhooks()):
                     # check if it has 'state attached' and is the bots webhook
-                    if await self.is_bot_webhook(webhook):
+                    if await self._is_bot_webhook(webhook):
                         bot_webhook = webhook
                 
                 if not bot_webhook:
@@ -144,7 +146,7 @@ class Donut(discord.ext.commands.Bot):
                         raise self.errors.WebhookError('Guild is not registered with any configuration')
                     guild_settings = dict(guild_settings[0])
                     bot_webhook = await channel.create_webhook(name=guild_settings['name'], avatar=guild_settings['avatar'])
-                await self.cache_channel_webhook(channel, bot_webhook)
+                await self._store_channel_webhook(channel, bot_webhook)
             
             # assign webhook object to channel for ease and then return it 
             self.webhooks[channel.id] = bot_webhook
@@ -152,7 +154,7 @@ class Donut(discord.ext.commands.Bot):
 
     async def delete_guild_webhooks(self, guild: discord.Guild, reason: str = None):
         for webhook in (await guild.webhooks()):
-            if not await self.is_bot_webhook():
+            if not await self._is_bot_webhook():
                 continue
             else:
                 await webhook.delete(reason=reason)
@@ -188,18 +190,45 @@ class Donut(discord.ext.commands.Bot):
             async def _():
                 with open('./main.html', 'r') as file:
                     return Response(file.read(), content_type='text/html')
+                
+            async def _event(self, **kwargs):
+                guild = self.bot.get_guild(kwargs['guild_id'])
+                if not guild:
+                    return Response(status=400, response='Invalid Guild', mimetype='application/json')
+                event = await (await self.bot.sqlite.execute('SELECT * FROM events WHERE guild_id = ? AND event_id = ?')).fetchone()
+                guild_event = guild.get_scheduled_event(kwargs['event_id'])
+                if not event or guild_event:
+                    return Response(status=400, response='Invalid Event', mimetype='application/json')
+
+                if request.args.get('data_only').lower() == 'true':
+                    pass
+                
+                if kwargs['title']:
+                    if not kwargs['title'] == event[1]: # the title
+                        return redirect(location=request.url.replace(kwargs['title'], event[1]))
             
             @self.app.route('/event/<int:guild_id>/<int:event_id>')
-            async def _event(guild_id, event_id):
-                guild = self.bot.get_guild(guild_id)
-                if not guild: return Response(response='Invalid Guild')
-                event = guild.get_scheduled_event(event_id)
-                if not event: return Response(response='Invalid Event ID')
+            async def _event1(guild_id, event_id):
+                await _event(guild_id=guild_id, event_id=event_id)
+            @self.app.route('/event/<string:title>/<int:guild_id>/<int:event_id>')
+            async def _event1(title, guild_id, event_id):
+                await _event(title=title, guild_id=guild_id, event_id=event_id)
             
-
 
 async def main():
     bot = Donut()
-    tasks = [bot.quart.run(), bot.start(os.environ.get('TOKEN'))]
-    await asyncio.gather(*tasks)
+    redis = redis.Redis(host=os.environ.get('REDISHOST'),
+                        port=os.environ.get('REDISPORT'),
+                        password=os.environ.get('REDISPASSWORD'),
+                        username=os.environ.get('REDISUSER'),
+                        decode_responses=True)
+    async with asyncpg.create_pool(database=os.environ.get('PGDATABASE'),
+                                   host=os.environ.get('PGHOST'),
+                                   user=os.environ.get('PGUSER'),
+                                   password=os.environ.get('PGPASSWORD'),
+                                   port=os.environ.get('PGPORT')) as psql:
+        bot.psql = psql
+        bot.redis = redis
+        tasks = [bot.quart.run(), bot.start(os.environ.get('TOKEN'))]
+        await asyncio.gather(*tasks)
 asyncio.run(main())
